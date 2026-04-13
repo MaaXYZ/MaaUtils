72c94c90570310125a7865cf16f6cbccf6873bc8
MistEO <mistereo@hotmail.com>
2026-04-13 17:56:51 +0800

    chore: rename log

--- include/MaaUtils/Logger.h
+++ include/MaaUtils/Logger.h
@@ -8,8 +8,8 @@ MAA_LOG_NS_BEGIN
 class MAA_UTILS_API Logger
 {
 public:
-    static constexpr std::string_view kLogFilename = "maa.log";
-    static constexpr std::string_view kLogbakFilename = "maa.bak.{}.log";
+    static constexpr std::string_view kLogFilename = "maafw.log";
+    static constexpr std::string_view kLogbakFilename = "maafw.bak.{}.log";
 
 public:
     static Logger& get_instance();
